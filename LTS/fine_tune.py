393174ccab39f48eb20f7ba109668e0581f0128e
Nikos Prasinos <nprasinos99@gmail.com>
2026-03-23 13:53:36 -0400

    weight decay update

--- LTS/fine_tune.py
+++ LTS/fine_tune.py
@@ -20,7 +20,7 @@ class BertFineTuner:
         self.trainer = None
         self.run_clf = False
         self.learning_rate = learning_rate
-        self.weight_decay = None
+        self.weight_decay = 0.01
         if dropout:
             model = BertForSequenceClassification.from_pretrained(model_name, num_labels=2)
             model.config.hidden_dropout_prob = dropout
