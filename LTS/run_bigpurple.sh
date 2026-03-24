1da4167eeaab1fde05b96791273adc92d207e8a4
Nikos Prasinos <nprasinos99@gmail.com>
2026-03-23 23:32:03 -0400

    Slurm sbatch script updated

--- LTS/run_bigpurple.sh
+++ LTS/run_bigpurple.sh
@@ -4,8 +4,9 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32GB
+#SBATCH --partition=gpu4_short
 #SBATCH --gres=gpu:1
-#SBATCH --time=08:00:00
+#SBATCH --time=04:00:00
 #SBATCH --output=/gpfs/scratch/np3106/DE_Project/DataEngFinal/LTS/lts_%j.out
 #SBATCH --error=/gpfs/scratch/np3106/DE_Project/DataEngFinal/LTS/lts_%j.err
 
@@ -16,17 +17,17 @@ module purge
 source ~/.bashrc
 conda activate /gpfs/scratch/np3106/venvs/lts
 
-# Run LTS with HuggingFace model on shark dataset
+# Run LTS with HuggingFace model on leather dataset
 python main_cluster.py \
     -sample_size 200 \
-    -filename "data_use_cases/shark_trophy" \
-    -val_path "data_use_cases/validation_sharks.csv" \
+    -filename "data_use_cases/data_leather" \
+    -val_path "data_use_cases/leather_validation.csv" \
     -balance False \
     -sampling "thompson" \
     -filter_label True \
     -model_finetune "bert-base-uncased" \
     -labeling "huggingface" \
-    -hf_model "mistralai/Mistral-7B-Instruct-v0.3" \
+    -hf_model "Qwen/Qwen2.5-3B-Instruct" \
     -model "text" \
     -baseline 0.5 \
     -metric "f1" \
